17dd96e48fcfe47b5e90885f604f420ecb1370ed
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-06-23 23:34:44 -0300

    feat: adicionando o componente Video

---
+++ src/components/Video.tsx
@@ -0,0 +1,103 @@
+import { Lightning, DiscordLogo, FileArrowDown, CaretRight, Image } from 'phosphor-react';
+import { DefaultUi, Player, Youtube } from '@vime/react';
+
+import '@vime/core/themes/default.css';
+
+export function Video() {
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1092px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="SO4-izct7Mc" />
+            <DefaultUi />
+          </Player>
+        </div>
+      </div>
+
+      <div className="p-8 max-w-[1092px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold mb-4">
+              Aula 01 - Criando o projeto e realizando o setup inicial
+            </h1>
+            <p className="text-gray-200 leading-relaxed">
+              Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação
+              utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto
+              no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela
+              plataforma no nosso front-end utilizando Apollo Client.
+            </p>
+
+            <div className="mt-6 flex items-center gap-4">
+              <img
+                src="https://github.com/diego3g.png"
+                alt=""
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+              />
+
+              <div className="leading-relaxed">
+                <strong className="text-2xl font-bold block">Diego Fernandes</strong>
+                <span className="text-sm text-gray-200 block">Co-fundador e CTO na Rocketseat</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <a
+              href="#"
+              className="p-4 bg-green-500 text-sm font-bold uppercase rounded flex items-center gap-[10px] hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade no Discord
+            </a>
+
+            <a
+              href="#"
+              className="p-4 text-sm font-bold uppercase text-blue-500 border border-blue-500 rounded flex items-center justify-center gap-[10px] hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+        <div className="mt-20 gap-8 grid grid-cols-2">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-start gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <h1 className="text-2xl font-bold mb-2">Material complementar</h1>
+              <p className="text-sm text-gray-200">
+                Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} color="#81D8F7" />
+            </div>
+          </a>
+
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-start gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <Image size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <h1 className="text-2xl font-bold mb-2">Wallpapers exclusivos</h1>
+              <p className="text-sm text-gray-200">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} color="#81D8F7" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Platform.tsx
+++ src/pages/Platform.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
+import { Video } from '../components/Video';
 
 export function Platform() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
+        <Video />
         <Sidebar />
       </main>
     </div>

d2ee7276e3f9a428bdace11ca2972a0f9cc290ef
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-06-26 18:40:58 -0300

    refactor: removendo o console.log()

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,8 +26,6 @@ const GET_LESSONS_QUERY = gql`
 export function Sidebar() {
   const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
 
-  console.log(data);
-
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="text-2xl font-bold pb-6 mb-6 border-b border-gray-500 block">
